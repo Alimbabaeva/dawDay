8e16f59cbeef8d2749a248da813c531014d9738f
Nurzat <alimbabaevanurzat@gmail.com>
2024-04-11 17:19:38 -0700

    first commit

---
+++ draw.py
@@ -0,0 +1,21 @@
+import turtle
+# 5 times
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+turtle.forward(100)
+turtle.right(144)
+
+
+
+turtle.done()
